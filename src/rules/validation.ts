1768b3ec2e28ba41212c8405a1a350cbd9174891
Nikesh Sapkota <nikeshdev0@gmail.com>
2023-01-19 15:16:09 +0545

    feat: add login validation

--- src/rules/validation.ts
+++ src/rules/validation.ts
@@ -18,3 +18,18 @@ export const signupValidationSchema = yup.object().shape({
     .minSymbols(1, "password must contain at least 1 special character"),
   acceptAggrement: yup.bool().oneOf([true], "Please accept our policies"),
 });
+
+export const loginValidationSchema = yup.object().shape({
+  email: yup.string().required("Email is required").email("Email is invalid"),
+  password: yup
+    .string()
+    .required(
+      "Use 6 or more characters that contain a mixture of letters, digits, and symbols."
+    )
+    .min(6, "Password must be at least 6 characters")
+    .max(15, "Password must not exceed 15 characters")
+    .minLowercase(1, "password must contain at least 1 lower case letter")
+    .minUppercase(1, "password must contain at least 1 upper case letter")
+    .minNumbers(1, "password must contain at least 1 number")
+    .minSymbols(1, "password must contain at least 1 special character"),
+});
