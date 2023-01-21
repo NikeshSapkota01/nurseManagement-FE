5bcc0721b3483c90e395cfa1b425d5fb62aca49f
Nikesh Sapkota <nikeshdev0@gmail.com>
2023-01-21 16:42:22 +0545

    feat: add refresh and access token interceptors

--- src/utils/httpUtils.ts
+++ src/utils/httpUtils.ts
@@ -1,4 +1,12 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
+
+import {
+  getRefreshToken,
+  saveAccessToken,
+  getAccessToken,
+  removeTokens,
+} from "@/services/token";
+import endpoints from "src/constants/endpoint";
 
 const http = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
@@ -76,3 +84,54 @@ export function remove(url: string, data = {}) {
 
   return http(request);
 }
+
+/**
+ * Auto adds the bearer token in Authorization Header.
+ */
+const requestInterceptor = async (request: AxiosRequestConfig<any>) => {
+  const accessToken = getAccessToken();
+
+  if (accessToken) {
+    request.headers["Authorization"] = `Bearer ${accessToken}`;
+  }
+
+  return request;
+};
+
+/**
+ * Gets accessToken from refreshToken
+ * If access token can't be get then clears the tokens
+ */
+export async function responseInterceptor(err: any) {
+  const error = err?.response || err;
+
+  const originalRequest = error.config;
+
+  const refreshToken = getRefreshToken();
+
+  if (!refreshToken) return removeTokens();
+
+  if (error.status === 401 && error.data.message === "Unauthorized access!") {
+    try {
+      return http
+        .post(endpoints.auth.token, { refreshToken })
+        .then(({ data }) => {
+          saveAccessToken(data.data.accessToken);
+          axios.defaults.headers.common[
+            "Authorization"
+          ] = `Bearer ${data.data.accessToken}`;
+          originalRequest.headers[
+            "Authorization"
+          ] = `Bearer ${data.data.accessToken}`;
+          return http(originalRequest);
+        });
+    } catch (error) {
+      removeTokens();
+    }
+  }
+
+  return Promise.reject(error);
+}
+
+http.interceptors.request.use(requestInterceptor);
+http.interceptors.response.use((response) => response, responseInterceptor);
