84f01399bc52e7eb69c6fe06a3fad8e0726ef95a
Nikesh Sapkota <nikeshdev0@gmail.com>
2023-01-30 18:48:53 +0545

    feat: add interface GetNurseResponseData and FetchNurseDataType

--- src/constants/interface.ts
+++ src/constants/interface.ts
@@ -15,3 +15,31 @@ export interface ValueOption {
   value: string;
   label: string;
 }
+
+export interface GetNurseResponseData {
+  data: {
+    0: {
+      id: number;
+      firstName: string;
+      middleName: string;
+      lastName: string;
+      email: string;
+      contact: string;
+      working_days: any;
+      duty_start_time: string;
+      duty_end_time: string;
+      isRoundingManager: boolean;
+      image: string;
+      created_at: string;
+      created_by: number;
+      is_deleted: boolean;
+    };
+  };
+}
+export interface FetchNurseDataType {
+  data: any;
+  status: "idle" | "loading" | "succeeded" | "failed" | "fullfilled";
+  error: string | null | undefined;
+  individualData: any;
+  individualDataStatus: "loading" | "succeeded" | "failed";
+}
